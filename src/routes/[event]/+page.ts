8754db024c78d2e6a93e77a04f60ac587bc480f7
Henahax <mail@henahax.de>
2024-04-07 12:02:29 +0200

    wip

--- src/routes/[event]/+page.ts
+++ src/routes/[event]/+page.ts
@@ -1,6 +1,7 @@
 export async function load({ params }) {
     let data = undefined;
     try{
+        console.log(`../../events/${params.event}/event.json`)
         data = await import(`../../events/${params.event}/event.json`);
         return data.default;
     }catch(error){
