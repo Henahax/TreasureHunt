3cf3448e77f12a4d42ef28e029af768977f0105f
Henahax <mail@henahax.de>
2024-04-08 00:27:01 +0200

    wip

--- src/routes/[event]/+page.svelte
+++ src/routes/[event]/+page.svelte
@@ -25,47 +25,55 @@
 	}
 
 	// Open Task, if last unlocked and next no password -> unlock next
-	function test(event) {
-		console.log(event.detail);
+	function toggle(event) {
+		for (let i = 0; i < $eventStore.tasks.length; i++) {
+			if ($eventStore.tasks[i].id === event.detail.id) {
+				if (
+					$eventStore.tasks[i + 1] !== undefined &&
+					$eventStore.tasks[i + 1].password === undefined
+				) {
+					$eventStore.tasks[i + 1].locked = false;
+				}
+				break;
+			}
+		}
 	}
 </script>
 
 {#if data.id}
 	<h2 class="h3">Quest Log</h2>
-	<div class="w-full card">
-		<Accordion width="w-full">
-			{#each $eventStore.tasks as task, t}
-				<AccordionItem bind:disabled={task.locked} on:toggle={test}>
-					<svelte:fragment slot="lead">
-						{#if task.active}
-							<i class="fa-solid fa-circle-exclamation animate-pulse text-success-500"></i>
-						{:else if task.locked}
-							<i class="fa-solid fa-lock"></i>
-						{:else}
-							<i class="fa-solid fa-check"></i>
+	<Accordion width="w-full" autocollapse class="text-token card">
+		{#each $eventStore.tasks as task}
+			<AccordionItem id={task.id} bind:disabled={task.locked} on:toggle={toggle}>
+				<svelte:fragment slot="lead">
+					{#if task.active}
+						<i class="fa-solid fa-circle-exclamation animate-pulse text-success-500"></i>
+					{:else if task.locked}
+						<i class="fa-solid fa-lock"></i>
+					{:else}
+						<i class="fa-solid fa-check"></i>
+					{/if}
+				</svelte:fragment>
+				<svelte:fragment slot="summary">
+					<div class:text-success-500={task.active}>
+						<p class="text-sm">{task.name}</p>
+						{#if !task.locked}
+							<p>{task.description}</p>
 						{/if}
-					</svelte:fragment>
-					<svelte:fragment slot="summary">
-						<div class={task.active ? 'text-success-500' : ''}>
-							<p class="text-sm">{task.name}</p>
-							{#if !task.locked}
-								<p>{task.description}</p>
-							{/if}
-						</div>
-					</svelte:fragment>
-					<svelte:fragment slot="content">
-						{#each task.content as content}
-							{#if content.type === 'text'}
-								<p>{@html content.text}</p>
-							{:else if content.type === 'image'}
-								<img src={content.src} alt="" />
-							{/if}
-						{/each}
-					</svelte:fragment>
-				</AccordionItem>
-			{/each}
-		</Accordion>
-	</div>
+					</div>
+				</svelte:fragment>
+				<svelte:fragment slot="content">
+					{#each task.content as content}
+						{#if content.type === 'text'}
+							<p>{@html content.text}</p>
+						{:else if content.type === 'image'}
+							<img src={content.src} alt="" />
+						{/if}
+					{/each}
+				</svelte:fragment>
+			</AccordionItem>
+		{/each}
+	</Accordion>
 {:else}
 	<div class="text-center text-error-500">
 		<h2 class="h2">not found</h2>
