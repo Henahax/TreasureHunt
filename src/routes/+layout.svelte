b4515bee99bd8bfa1ef9274eac830412821cc3ad
Henahax <mail@henahax.de>
2024-04-30 21:36:48 +0200

    Nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,41 @@
 	import './styles.css';
 </script>
 
-<div class="app">
+<div class="app min-h-dvh">
 	<main>
 		<slot />
 	</main>
 
-	<footer></footer>
+	<div class="absolute bottom-0 flex flex-row p-4 justify-center w-full items-center gap-4">
+		<nav>
+			<ul class="flex flex-row">
+				<li>
+					<a href="/" class="btn btn-active btn-neutral">
+						<i class="fa-solid fa-house"></i>
+					</a>
+				</li>
+				<li>
+					<a href="/" class="btn btn-active btn-neutral">
+						<i class="fa-solid fa-house"></i>
+					</a>
+				</li>
+				<li>
+					<a href="/" class="btn btn-active btn-neutral">
+						<i class="fa-solid fa-house"></i>
+					</a>
+				</li>
+				<li>
+					<a href="/" class="btn btn-active btn-neutral">
+						<i class="fa-solid fa-house"></i>
+					</a>
+				</li>
+			</ul>
+		</nav>
+
+		<button class="btn btn-circle btn-primary btn-lg text-2xl">
+			<i class="fa-solid fa-key"></i>
+		</button>
+	</div>
 </div>
 
 <style>
